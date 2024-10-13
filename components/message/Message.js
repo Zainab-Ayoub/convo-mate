c94a418822b31a8f343f21dacbf56ff95482bda8
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-10-14 00:12:16 +0500

    message

--- components/message/Message.js
+++ components/message/Message.js
@@ -1,6 +1,21 @@
+import { useUser } from "@auth0/nextjs-auth0/client"
+import Image from "next/image"
+
 export const Message = ({role, content}) => {
+    const { user } = useUser();
+    console.log("USER: ", user);
     return <div className="grid grid-cols-[30px_1fr] gap-5 p-5">
-        <div>avatar</div>
+        <div>
+            {role === "user" && !!user && (
+                <Image
+                  src={user.picture}
+                  width={30}
+                  height={30}
+                  alt="User avatar"
+                  className="rounded-sm shadow-sm shadow-black/50"
+                />
+            )}
+        </div>
         <div>{content}</div>
     </div>
 }
