57405044f39c712b9332cab1a9ae345c355142cc
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-10-14 18:35:23 +0500

    new chat

--- pages/api/chat/createNewChat.js
+++ pages/api/chat/createNewChat.js
@@ -11,7 +11,15 @@ export default async function handler(req, res) {
       };
       const client = await clientPromise;
       const db = client.db("ConvoMate");     
+      const chat = await db.collection("chats").insertOne({
+        userId: user.sub,
+        messages: [newUserMessage],
+        title: message,
+      });
     } catch (e) {
+        res
+          .status(500)
+          .json({ message: "An error occured when creating a new chat"});
       console.log("ERROR OCCURED IN CREATE NEW CHAT: ", e);
     }
 }
