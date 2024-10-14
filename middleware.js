163257764ea37e243476054f96e5dcc01bd01bcf
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-10-14 14:54:59 +0500

    /chat/

--- middleware.js
+++ middleware.js
@@ -3,5 +3,5 @@ import { withMiddlewareAuthRequired } from "@auth0/nextjs-auth0";
 export default withMiddlewareAuthRequired();
 
 export const config = {
-    matcher: ["/api/chat/:path*"],
+    matcher: ["/api/chat/:path*", "/chat/:path*"],
 };
